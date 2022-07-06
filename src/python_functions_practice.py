b7ec26372d320e3975dd7fc25c2ab187786f8ec4
Alejandro Garcia <alejandrojg8@gmail.com>
2022-07-06 16:32:17 +0100

    homework lab

---
+++ src/python_functions_practice.py
@@ -0,0 +1,45 @@
+def return_10():
+    return 10
+
+def add(a,b):
+    return a + b  
+
+def subtract(a,b):
+    return a - b  
+
+
+def multiply(a,b):
+    answer = a * b
+    return answer
+    
+
+    #If I print(answer) would give me 8
+
+def divide(a,b):
+   answer = a / b
+   return answer
+
+def length_of_string(a_string):
+    answer = len(a_string) 
+    return answer
+
+def join_string(a,b):
+    return a + b
+
+def add_string_as_number(a,b):
+    return int(a) + int(b)
+
+def number_to_full_month_name(month):
+    calendar = ["January","February", "March", 
+    "April", "May", "June", "July", "August", 
+    "September", "October", "November", "December"]
+    return calendar[month-1]
+
+def number_to_short_month_name(month):
+    calendar = ["January","February", "March", 
+    "April", "May", "June", "July", "August", 
+    "September", "October", "November", "December"]
+    return calendar[month-1]
+
+
+
